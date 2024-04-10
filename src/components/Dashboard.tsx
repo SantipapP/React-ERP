6a4ae54960103c92c6679452eadb2b5667d74982
ITLcitDepartment <89773041+SantipapP@users.noreply.github.com>
2024-04-10 14:45:34 +0700

    Edit Dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -19,22 +19,14 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      <div className="container m-20">
-        <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="container m-10">
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Card title!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Card title!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <button className="btn btn-primary">Listen</button>
             </div>
           </div>
         </div>
